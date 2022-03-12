17b19fc52e96414095f57bd16db8c34cb23cb048
Mohammad Abbas <mohdabbas@narbase.com>
2022-03-12 12:50:10 +0400

    Start a 30 sec countdown timer when click start button and update the timer text with th count and show a toast message when the countdown is finished

--- app/src/main/java/com/mohdabbas/stopwatchtimer/ui/TimerFragment.kt
+++ app/src/main/java/com/mohdabbas/stopwatchtimer/ui/TimerFragment.kt
@@ -1,22 +1,58 @@
 package com.mohdabbas.stopwatchtimer.ui
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.mohdabbas.stopwatchtimer.R
+import com.mohdabbas.stopwatchtimer.databinding.FragmentTimerBinding
+import java.util.concurrent.TimeUnit
 
 class TimerFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
+    private var binding: FragmentTimerBinding? = null
+    private var totalTimeInMillis = 30000L
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false)
+        binding = FragmentTimerBinding.inflate(inflater, container, false)
+        binding?.timerText?.text = getTimeText(totalTimeInMillis)
+        binding?.setupButtons()
+        return binding?.root
+    }
+
+    private fun FragmentTimerBinding.setupButtons() {
+        startCancelButton.setOnClickListener { onStartCancelClicked(totalTimeInMillis) }
+    }
+
+    private fun onStartCancelClicked(totalTimeInMillis: Long) {
+        object : CountDownTimer(totalTimeInMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                binding?.timerText?.text = getTimeText(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                Toast.makeText(context, "Done", Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+    private fun getTimeText(time: Long): String {
+        return String.format(
+            "%02d:%02d:%02d",
+            TimeUnit.MILLISECONDS.toHours(time),
+            TimeUnit.MILLISECONDS.toMinutes(time),
+            TimeUnit.MILLISECONDS.toSeconds(time)
+        )
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
