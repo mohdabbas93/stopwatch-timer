1492ee8c2deabb052d3e4db4db248ae20d07806e
Mohammad Abbas <mohdabbas@narbase.com>
2022-03-12 14:51:38 +0400

    Add Pause/Resume functionality

--- app/src/main/java/com/mohdabbas/stopwatchtimer/ui/TimerFragment.kt
+++ app/src/main/java/com/mohdabbas/stopwatchtimer/ui/TimerFragment.kt
@@ -29,10 +29,12 @@ class TimerFragment : Fragment() {
 
     private fun FragmentTimerBinding.setupButtons() {
         startCancelButton.setOnClickListener { onStartCancelClicked(totalTimeInMillis) }
+        pauseResumeButton.setOnClickListener { onPauseResumeButtonClicked() }
     }
 
     private var isTimerStarted = false
     private lateinit var countDownTimer: CountDownTimer
+    private var remainingMilliseconds = 0L
 
     private fun onStartCancelClicked(totalTimeInMillis: Long) {
         if (!isTimerStarted) {
@@ -41,6 +43,7 @@ class TimerFragment : Fragment() {
 
             countDownTimer = object : CountDownTimer(totalTimeInMillis, 1000) {
                 override fun onTick(millisUntilFinished: Long) {
+                    remainingMilliseconds = millisUntilFinished
                     binding?.timerText?.text = getTimeText(millisUntilFinished)
                 }
 
@@ -53,6 +56,29 @@ class TimerFragment : Fragment() {
             countDownTimer.cancel()
             binding?.startCancelButton?.text = getString(R.string.start)
             binding?.timerText?.text = getTimeText(totalTimeInMillis)
+        }
+    }
+
+    private var isTimerPaused = false
+
+    private fun onPauseResumeButtonClicked() {
+        if (isTimerPaused) {
+            isTimerPaused = false
+            binding?.pauseResumeButton?.text = getString(R.string.pause)
+            countDownTimer = object : CountDownTimer(remainingMilliseconds, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    remainingMilliseconds = millisUntilFinished
+                    binding?.timerText?.text = getTimeText(millisUntilFinished)
+                }
+
+                override fun onFinish() {
+                    Toast.makeText(context, "Done", Toast.LENGTH_SHORT).show()
+                }
+            }.apply { start() }
+        } else {
+            isTimerPaused = true
+            countDownTimer.cancel()
+            binding?.pauseResumeButton?.text = getString(R.string.resume)
         }
     }
 
