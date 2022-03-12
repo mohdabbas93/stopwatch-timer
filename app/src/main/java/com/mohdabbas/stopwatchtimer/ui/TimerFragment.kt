d5af69dc307c8c8b991a085fe684019167145c5e
Mohammad Abbas <mohdabbas@narbase.com>
2022-03-12 16:04:46 +0400

    Hide the pause/resume button when the countdown started and hide it when the countdown canceled

--- app/src/main/java/com/mohdabbas/stopwatchtimer/ui/TimerFragment.kt
+++ app/src/main/java/com/mohdabbas/stopwatchtimer/ui/TimerFragment.kt
@@ -10,6 +10,8 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.mohdabbas.stopwatchtimer.R
 import com.mohdabbas.stopwatchtimer.databinding.FragmentTimerBinding
+import com.mohdabbas.stopwatchtimer.util.makeGone
+import com.mohdabbas.stopwatchtimer.util.makeVisible
 import java.util.concurrent.TimeUnit
 
 class TimerFragment : Fragment() {
@@ -40,14 +42,24 @@ class TimerFragment : Fragment() {
     private fun onStartCancelClicked(totalTimeInMillis: Long) {
         if (!isTimerStarted) {
             isTimerStarted = true
+            showPauseResumeButton()
             updateStartCancelButtonText(R.string.cancel)
             initializeAndStartCountDownTimer(totalTimeInMillis)
         } else {
             isTimerStarted = false
+            hidePauseResumeButton()
             countDownTimer.cancel()
             updateStartCancelButtonText(R.string.start)
             binding?.timerText?.text = getTimeText(totalTimeInMillis)
         }
+    }
+
+    private fun showPauseResumeButton() {
+        binding?.pauseResumeButton?.makeVisible()
+    }
+
+    private fun hidePauseResumeButton() {
+        binding?.pauseResumeButton?.makeGone()
     }
 
     private var isTimerPaused = false
---
+++ app/src/main/java/com/mohdabbas/stopwatchtimer/util/VisibilityUtil.kt
@@ -0,0 +1,16 @@
+package com.mohdabbas.stopwatchtimer.util
+
+import android.view.View
+
+/**
+ * Created by Mohammad Abbas
+ * On: 3/12/22.
+ */
+
+fun View.makeVisible() {
+    visibility = View.VISIBLE
+}
+
+fun View.makeGone() {
+    visibility = View.GONE
+}
